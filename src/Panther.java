b48d0dfb22e57924208158d2a67166322868bfe7
gracegerwe <gracegerwe@10.31.6.79>
2018-01-19 15:02:35 +0000

    added Panther class

---
+++ src/Panther.java
@@ -0,0 +1,21 @@
+
+public class Panther extends Animal {
+
+	public Panther(String name) {
+		// TODO Auto-generated constructor stub
+		super(name);
+	}
+	
+	public String getSound() {
+		return "growl";
+	}
+
+	public boolean hasWings() {
+		return false;
+	}
+
+	public  int getNumLegs() {
+		return 4;
+	}
+
+}
